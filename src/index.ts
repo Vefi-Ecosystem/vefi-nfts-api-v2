f7b5a64a9703586c05da551d7f6ad8cd915b35bc
Kingsley Victor <javaprodigy56@gmail.com>
2022-11-08 17:04:49 +0100

    ...

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const app = express();
+const port = parseInt(process.env.PORT || '55019');
+
+app.listen(port, () => {
+  console.log(`App listening on ${port}`);
+});
